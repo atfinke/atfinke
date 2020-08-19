1ce32a14642fb6961fa99da9171f0cf35bcbe791
Andrew Finke <andrewfinke@me.com>
2020-08-19 09:32:09 -0500

    track count

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ parser.add_argument("API_KEY")
 parser.add_argument("API_SECRET")
 parser.add_argument("USERNAME")
 parser.add_argument("PASSWORD")
+parser.add_argument("TRACKS")
 args = parser.parse_args()
 
 password_hash = pylast.md5(args.PASSWORD)
 network = pylast.LastFMNetwork(api_key=args.API_KEY, api_secret=args.API_SECRET,
                                username=args.USERNAME, password_hash=password_hash)
 
-tracks = network.get_user(args.USERNAME).get_recent_tracks(limit=5)
+tracks = network.get_user(args.USERNAME).get_recent_tracks(limit=args.TRACKS)
 
 image_size = "16%"
 output = "### Recent Tracks\n"
 
-for playedTrack in tracks:
+for index, playedTrack in enumerate(tracks):
     track = playedTrack.track
     
     track_name = track.get_name()
@@ -34,6 +35,8 @@ for playedTrack in tracks:
 
 
     output += "[<img src='{}' width='{}' height='{}' alt='{}'>]({})&nbsp;&nbsp;&nbsp;&nbsp;".format(image_url, image_size, image_size, track_name.replace("'", ""), track_url)
+    if index % 5 == 0 and index != 0:
+        output += "<br>"
 
 with open("README.md", "w") as file:
     file.write(output)
